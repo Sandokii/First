4f1af08624f800782482422baad4ab11872ab1c9
Valonaute <121163863+Valonaute@users.noreply.github.com>
2023-02-03 13:47:46 +0100

    Create index1.php

---
+++ index1.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Coaching de vie & Confiance en soi</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="Aidez à atteindre vos objectifs de vie avec notre coaching de vie et de confiance en soi">
+  <meta name="keywords" content="coaching de vie, confiance en soi, objectifs">
+</head>
+
+<body>
+  <header>
+    <h1>Coaching de vie & Confiance en soi</h1>
+    <nav>
+      <ul>
+        <li><a href="#presentation">Présentation</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#temoignages">Témoignages</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <section id="presentation">
+      <h2>Présentation</h2>
+      <p>Bienvenue sur notre site consacré au coaching de vie et à la confiance en soi. Nous aidons les gens à atteindre leurs objectifs de vie en renforçant leur confiance en eux-mêmes.</p>
+    </section>
+
+    <section id="services">
+      <h2>Services</h2>
+      <ul>
+        <li>Coaching de vie</li>
+        <li>Coaching de confiance en soi</li>
+        <li>Atteinte d'objectifs</li>
+      </ul>
+    </section>
+
+    <section id="temoignages">
+      <h2>Témoignages</h2>
+      <ul>
+        <li>
+          <h3>Client 1</h3>
+          <p>"Le coaching m'a vraiment aidé à trouver la confiance en moi pour atteindre mes objectifs de vie."</p>
+        </li>
+        <li>
+          <h3>Client 2</h3>
+          <p>"Le coaching de vie m'a permis de mieux comprendre mes objectifs et comment les atteindre."</p>
+        </li>
+      </ul>
+    </section>
+  </main>
+  <footer>
+    <p> Merci de votre visite </p
+  </footer>
+</body>
